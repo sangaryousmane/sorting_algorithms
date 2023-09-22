e8c105b26fc4f94626e9e2b0b0247f4ead1b1bfe
BruceIsBat <awoteduibrahim@gmail.com>
2023-09-22 13:21:03 -0700

    betty check updated

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -15,7 +15,7 @@ void selection_sort(int *array, size_t size)
 	for (i = 0; i < size - 1; i++)
 	{
 		min_idx = i;
-		
+
 		for (j = i + 1; j < size; j++)
 		{
 			if (array[j] < array[min_idx])
@@ -28,7 +28,7 @@ void selection_sort(int *array, size_t size)
 			array[i] = array[min_idx];
 			array[min_idx] = temp;
 
-            /* Print the array after each swap */
+		/* Print the array after each swap */
 			for (j = 0; j < size - 1; j++)
 				printf("%d, ", array[j]);
 			printf("%d\n", array[size - 1]);
