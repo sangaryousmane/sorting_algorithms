b2de1e5bc3dbaeb8995a9db2009885eaf7fa2de0
BruceIsBat <awoteduibrahim@gmail.com>
2023-09-22 13:08:29 -0700

    updated the header file

--- sort.h
+++ sort.h
@@ -31,4 +31,6 @@ void quick_sort_helper(int *nums, size_t size, int start, int end);
 /** SORTING FUNCTIONS **/
 void bubble_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
+void insertion_sort_list(listint_t **list);
+void selection_sort(int *array, size_t size);
 #endif /** _SORT_H **/
